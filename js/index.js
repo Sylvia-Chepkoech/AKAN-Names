2d9e7b08689f4d37cd97df5549cb945da0f7c965
Sylvia-Chepkoech <slycheche@gmail.com>
2022-03-08 02:00:16 +0300

    add changes to HTML

--- js/index.js
+++ js/index.js
@@ -1,53 +1,67 @@
 //Input = Birthday
 //Output = Akan Name(Day of the week)
 
-function birthdayHandler() {
+function generateAkanName (){
   let button = document.getElementById("submitButton")
-}
 
-button.addEventListener("click", function callback(event) {
-  let day = parseInt(document.getElementById("day-input").value)
-  let month = parseInt(document.getElementById("month-input").value)
-  let year = parseInt(document.getElementById("year-input").value)
-  let gender = document.getElementById("gender").value
+ button.addEventListener("click", function callBack(event){
+  
+  let dayOfBirth = parseInt(document.getElementById("day").value)
+  let monthOfBirth = parseInt(document.getElementById("month").value)
+  let yearOfBirth = parseInt(document.getElementById("year").value)
+  let MI = document.getElementsByName("gender")[0].checked
+  let FI = document.getElementsByName("gender")[1].checked
+  let gender = MI ? "male" : "female" 
   let finalStatementElement = document.getElementById("finalStatement")
-
+  
   let minYob = 1900
+  let maxYob = 2022
 
-  //Months
-  let months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-  "November", "December")
-  //Days
+  //Months of the year
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+  "November", "December"]
+  //Days of the week
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   //Akan Names
   let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   let maleAkanNames = ["Kwasi", "Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-  //If a day is greater tha 31 or less than 1. Prompt the user.
-  If (day) > 31 || < 1) {
+  //If a day is greater than 31 or less than 1. Prompt the user.
+  if (dayOfBirth > 31 || dayOfBirth < 1) {
     alert("Invalid date.")
     return false
-  };
-  //Month of February(exception)
-
-
-  If (month > 12 || < 1) {
+  }
+  if (monthOfBirth === 2 && dayOfBirth >29) {
+    alert("Invalid date. February < 29 days")
+      return false;  
+  }
+  //Month of February(Leap Year)
+  if(monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+    if(dayOfBirth > 28 || dayOfBirth < 1) {
+      alert("Invalid date.Leap year < 28 days")
+      return false; 
+    }
+    
+  }
+  
+  if (monthOfBirth > 12 || monthOfBirth < 1) {
     alert("Invalid month.")
     return false
   };
-
-  If (year < 1900) {
+  
+  if (yearOfBirth < minYob || yearOfBirth > maxYob) {
     alert("Invalid year.")
     return false
   };
 
-  var dob = new Date('${months[month - 1]} ${day}, ${year} 00:01:00');
+  var dob = new Date(`${months[monthOfBirth - 1]} ${dayOfBirth}, ${yearOfBirth} 00:01:00`);
+  
 
   let indexOfDay = dob.getDay();
 
   //Appropriate AKan Name
   let akanName =""
-  Let dayOfWeek = days[indexOfDay]
+  let dayOfWeek = days[indexOfDay]
 
   //Akan selection 
   switch(gender) {
@@ -58,11 +72,16 @@ button.addEventListener("click", function callback(event) {
       akanName = femaleAkanNames[indexOfDay]
       break;
 
-      default:
+    default:
       break;
   }
-
-  let finalStatement = "You were born on ${dayOfWeek} and your Akan Name is ${akanNames}
+  let finalStatement = `You were born on ${dayOfWeek} and your Akan Name is ${akanName}`;
 
   //set final sattement
-  finalStatementElement.innertext = finalStatement
+  finalStatementElement.innerText = finalStatement
+ })
+}
+
+generateAkanName()
+
+
